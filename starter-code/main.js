a43b95864fdc6978b0418e3c2f41e72cf36ce646
madwask <madwask@gmail.com>
2016-11-12 17:32:08 -0500

    Update main.js
    Created Java Script based on Unit 8 Lesson.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,13 @@
 console.log("JS file is connected to HTML! Woo!")
+/* define values for four cards; 2 are Queens, 2 are Kings. */
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+/* Test each of four cards to one another for value */
+cardOne == cardTwo;
+cardOne == cardThree;
+cardOne == cardFour;
+cardTwo == cardThree;
+cardTwo == cardFour;
+cardThree == cardFour;
